278fe319ea6a73042556e563b6d84673e27fb981
Andre Duarte <andremacielduarte@gmail.com>
2016-10-31 16:25:45 +0000

    Add configurable $interval delay

--- dist/ng-lazy-render.js
+++ dist/ng-lazy-render.js
@@ -161,106 +161,121 @@ angular.module('ngLazyRender').directive('lazyRepeater', ['$animate', '$compile'
     };
 }]);
 
-angular.module('ngLazyRender').service('VisibilityService', ['$interval', '$q', function ($interval, $q) {
-    var intervalPromise = void 0;
-    var watchingItems = [];
-    var idCounter = 0;
+angular.module('ngLazyRender').provider('VisibilityService', [function () {
+    var delay = 500;
 
-    // Code fully stolen from angular-inview. Thanks thenikso!
-    function getViewportHeight() {
-        var height, mode, ref;
-        height = window.innerHeight;
-        if (height) {
+    this.setDelay = function (newDelay) {
+        delay = newDelay;
+    };
+
+    this.$get = ['$interval', '$q', function ($interval, $q) {
+        var intervalPromise = void 0;
+        var watchingItems = [];
+        var idCounter = 0;
+
+        // Code fully stolen from angular-inview. Thanks thenikso!
+        function getViewportHeight() {
+            var height, mode, ref;
+            height = window.innerHeight;
+            if (height) {
+                return height;
+            }
+            mode = document.compatMode;
+            if (mode || !(typeof $ !== "undefined" && $ !== null ? (ref = $.support) != null ? ref.boxModel : void 0 : void 0)) {
+                height = mode === 'CSS1Compat' ? document.documentElement.clientHeight : document.body.clientHeight;
+            }
             return height;
         }
-        mode = document.compatMode;
-        if (mode || !(typeof $ !== "undefined" && $ !== null ? (ref = $.support) != null ? ref.boxModel : void 0 : void 0)) {
-            height = mode === 'CSS1Compat' ? document.documentElement.clientHeight : document.body.clientHeight;
-        }
-        return height;
-    }
 
-    // Code fully stolen from angular-inview. Thanks thenikso!
-    function getBoundingClientRect(element) {
-        var el, parent, top;
-        if (element.getBoundingClientRect != null) {
-            return element.getBoundingClientRect();
-        }
-        top = 0;
-        el = element;
-        while (el) {
-            top += el.offsetTop;
-            el = el.offsetParent;
-        }
-        parent = element.parentElement;
-        while (parent) {
-            if (parent.scrollTop != null) {
-                top -= parent.scrollTop;
+        // Code fully stolen from angular-inview. Thanks thenikso!
+        function getBoundingClientRect(element) {
+            var el, parent, top;
+            if (element.getBoundingClientRect != null) {
+                return element.getBoundingClientRect();
             }
-            parent = parent.parentElement;
-        }
-        return {
-            top: top,
-            bottom: top + element.offsetHeight
-        };
-    }
-
-    this._startWatching = function () {
-        if (!intervalPromise) {
-            intervalPromise = $interval(this._checkInView, 500, 0, false);
-        }
-    };
-
-    this._stopWatching = function () {
-        if (intervalPromise) {
-            $interval.cancel(intervalPromise);
-            intervalPromise = null;
-        }
-    };
-
-    // Code partially stolen from angular-inview. Thanks thenikso!
-    this._checkInView = function () {
-        var viewport = {
-            top: 0,
-            bottom: getViewportHeight()
-        };
-        var callbacks = [];
-
-        watchingItems.forEach(function (item) {
-            var bounds = getBoundingClientRect(item.element[0]);
-
-            if (bounds.top < viewport.bottom && bounds.bottom >= viewport.top) {
-                callbacks.push(item.callback);
+            top = 0;
+            el = element;
+            while (el) {
+                top += el.offsetTop;
+                el = el.offsetParent;
             }
-        });
+            parent = element.parentElement;
+            while (parent) {
+                if (parent.scrollTop != null) {
+                    top -= parent.scrollTop;
+                }
+                parent = parent.parentElement;
+            }
+            return {
+                top: top,
+                bottom: top + element.offsetHeight
+            };
+        }
 
-        callbacks.forEach(function (callback) {
-            callback();
-        });
-    };
+        function _startWatching() {
+            if (!intervalPromise) {
+                intervalPromise = $interval(this._checkInView, delay, 0, false);
+            }
+        }
 
-    this.whenVisible = function (element, scope, callback) {
-        var _this = this;
+        function _stopWatching() {
+            if (intervalPromise) {
+                $interval.cancel(intervalPromise);
+                intervalPromise = null;
+            }
+        }
 
-        var itemId = idCounter++;
+        // Code partially stolen from angular-inview. Thanks thenikso!
+        function _checkInView() {
+            var viewport = {
+                top: 0,
+                bottom: getViewportHeight()
+            };
+            var callbacks = [];
 
-        watchingItems.push({
-            element: element,
-            callback: callback,
-            scope: scope,
-            id: itemId
-        });
+            watchingItems.forEach(function (item) {
+                var bounds = getBoundingClientRect(item.element[0]);
 
-        scope.$on('$destroy', function () {
-            watchingItems = watchingItems.filter(function (item) {
-                return item.id !== itemId;
+                if (bounds.top < viewport.bottom && bounds.bottom >= viewport.top) {
+                    callbacks.push(item.callback);
+                }
             });
 
-            if (watchingItems.length === 0) {
-                _this._stopWatching();
-            }
-        });
+            callbacks.forEach(function (callback) {
+                callback();
+            });
+        }
 
-        this._startWatching();
-    };
+        function whenVisible(element, scope, callback) {
+            var _this = this;
+
+            var itemId = idCounter++;
+
+            watchingItems.push({
+                element: element,
+                callback: callback,
+                scope: scope,
+                id: itemId
+            });
+
+            scope.$on('$destroy', function () {
+                watchingItems = watchingItems.filter(function (item) {
+                    return item.id !== itemId;
+                });
+
+                if (watchingItems.length === 0) {
+                    _this._stopWatching();
+                }
+            });
+
+            this._startWatching();
+        }
+
+        return {
+            _startWatching: _startWatching,
+            _stopWatching: _stopWatching,
+            _checkInView: _checkInView,
+            whenVisible: whenVisible
+        };
+    }];
 }]);
