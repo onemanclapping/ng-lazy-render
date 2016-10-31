d40b7dcf06e58400f3abbbffb5d2b47eaf2b3d42
Andre Duarte <andremacielduarte@gmail.com>
2016-10-31 15:46:36 +0000

    Release 1.0.1 - let's hope this one sticks

--- dist/ng-lazy-render.js
+++ dist/ng-lazy-render.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * Module declaration
  */
@@ -17,69 +19,56 @@ angular.module('ngLazyRender', ['angular-inview']);
  *  <!-- lots of code -->
  * </any>
  */
-angular.module('ngLazyRender').directive('lazyModule', [
-    '$animate',
-    '$compile',
-    '$parse',
-    '$q',
-    '$templateCache',
-    'inViewDirective',
-    function ($animate, $compile, $parse, $q, $templateCache, inViewDirective) {
-        'use strict';
+angular.module('ngLazyRender').directive('lazyModule', ['$animate', '$compile', '$parse', '$q', '$templateCache', 'VisibilityService', function ($animate, $compile, $parse, $q, $templateCache, VisibilityService) {
+    'use strict';
 
-        return {
-            // 500 because is less than ngIf and ngRepeat
-            priority: 500,
-            terminal: true,
-            transclude: 'element',
-            link: function ($scope, $element, $attr, ctrl, $transclude) {
-                // If the expression in lazyIf is false, skip the directive's action
-                if ($parse($attr.lazyIf)($scope) === false) {
-                    $transclude(function (clone) {
-                        $animate.enter(clone, $element.parent(), $element);
-                    });
+    return {
+        // 500 because is less than ngIf and ngRepeat
+        priority: 500,
+        terminal: true,
+        transclude: 'element',
+        link: function link($scope, $element, $attr, ctrl, $transclude) {
+            // If the expression in lazyIf is false, skip the directive's action
+            if ($parse($attr.lazyIf)($scope) === false) {
+                $transclude(function (clone) {
+                    $animate.enter(clone, $element.parent(), $element);
+                });
+                return;
+            }
+
+            var el = angular.element($templateCache.get($attr.lazyModule));
+            var isolateScope = $scope.$new(true);
+
+            // Callback for VisibilityService to be called when the module becomes visible.
+            // This will destroy the scope of the placeholder and replace it with
+            // the actual transcluded content.
+            isolateScope.showModule = function () {
+                // If the function is called after the scope is destroyed (more than once),
+                // we should do nothing.
+                if (isolateScope === null) {
                     return;
                 }
+                // It is important to destroy the old scope or we'll never kill VisibilityService
+                isolateScope.$destroy();
+                isolateScope = null;
 
-                var el = angular.element($templateCache.get($attr.lazyModule));
-                var isolateScope = $scope.$new(true);
+                $transclude(function (clone) {
+                    var enterPromise = $animate.enter(clone, $element.parent(), $element);
+                    var leavePromise = $animate.leave(el);
 
-                // Callback for inViewDirective to be called when the module becomes visible.
-                // This will destroy the scope of the placeholder with inView and replace it with
-                // the actual transcluded content.
-                isolateScope.update = function () {
-                    // If the function is called after the scope is destroyed (more than once),
-                    // we should do nothing.
-                    if (isolateScope === null) {
-                        return;
-                    }
-                    // It is important to destroy the old scope or we'll get unwanted calls from
-                    // the inView directive.
-                    isolateScope.$destroy();
-                    isolateScope = null;
-
-                    $transclude(function (clone) {
-                        var enterPromise = $animate.enter(clone, $element.parent(), $element);
-                        var leavePromise = $animate.leave(el);
-
-                        $q.all([enterPromise, leavePromise]).then(function () {
-                            el = null;
-
-                            // This triggers inview again to make sure everything is checked again
-                            angular.element(window).triggerHandler('checkInView');
-                        });
-                    });
-                };
-
-                $animate.enter(el, $element.parent(), $element).then(function () {
-                    $compile(el)(isolateScope);
-                    inViewDirective[0].compile()(isolateScope, el, {
-                        inView: "$inview && update()"
+                    $q.all([enterPromise, leavePromise]).then(function () {
+                        el = null;
                     });
                 });
-            }
-        };
-    }]);
+            };
+
+            $animate.enter(el, $element.parent(), $element).then(function () {
+                $compile(el)(isolateScope);
+                VisibilityService.whenVisible(el, isolateScope, isolateScope.showModule);
+            });
+        }
+    };
+}]);
 
 /**
  * Use this directive as an attribute if you want a repeater (ng-repeat) to grow as the user scrolls down.
@@ -100,101 +89,178 @@ angular.module('ngLazyRender').directive('lazyModule', [
  *     </li>
  * </ul>
  */
-angular.module('ngLazyRender').directive('lazyRepeater', [
-    '$animate',
-    '$compile',
-    '$parse',
-    '$templateCache',
-    '$timeout',
-    function ($animate, $compile, $parse, $templateCache, $timeout) {
-        'use strict';
+angular.module('ngLazyRender').directive('lazyRepeater', ['$animate', '$compile', '$parse', '$templateCache', 'VisibilityService', function ($animate, $compile, $parse, $templateCache, VisibilityService) {
+    return {
+        restrict: 'A',
+        priority: 2000,
+        compile: function compile(tElement, tAttrs) {
+            var trackByIndex = tAttrs.ngRepeat.indexOf('track by');
 
-        return {
-            restrict: 'A',
-            priority: 2000,
+            if (trackByIndex === -1) {
+                tAttrs.ngRepeat += '| limitTo: getLazyLimit()';
+            } else {
+                tAttrs.ngRepeat = tAttrs.ngRepeat.substr(0, trackByIndex) + '| limitTo: getLazyLimit() ' + tAttrs.ngRepeat.substr(trackByIndex);
+            }
 
-            compile: function (tElement, tAttrs) {
-                var trackByIndex = tAttrs.ngRepeat.indexOf('track by');
+            var bufferProp = tAttrs.ngRepeat.match(/in (.*?)?([ |\n|]|$)/)[1];
 
-                if (trackByIndex === -1) {
-                    tAttrs.ngRepeat += "| limitTo: getLazyLimit()";
-                } else {
-                    tAttrs.ngRepeat = tAttrs.ngRepeat.substr(0, trackByIndex) +
-                        "| limitTo: getLazyLimit() " + tAttrs.ngRepeat.substr(trackByIndex);
+            return function ($scope, el, attrs) {
+                var limit = attrs.lazyRepeater;
+                var placeholderVisible = false;
+
+                function getBufferLength() {
+                    return $scope.$eval(bufferProp).length;
                 }
 
-                var bufferProp = tAttrs.ngRepeat.match(/in (.*?)?([ |\n|]|$)/)[1];
+                function addPlaceholder() {
+                    var placeholder = attrs.lazyPlaceholder ? $templateCache.get(attrs.lazyPlaceholder) || attrs.lazyPlaceholder : '';
+                    var placeholderEl = angular.element('<div>' + placeholder + '</div>');
+                    var isolateScope = $scope.$new(true);
 
-                return function ($scope, el, attrs) {
-                    var limit = attrs.lazyRepeater;
-                    var delay = parseInt(attrs.lazyDelay);
-                    var waiting = false;
-
-                    var placeholderVisible = false;
-
-                    function getBufferLength() {
-                        return $scope.$eval(bufferProp).length;
-                    }
-
-                    function addPlaceholder() {
-                        var placeholder = attrs.lazyPlaceholder ? $templateCache.get(attrs.lazyPlaceholder) || attrs.lazyPlaceholder : '';
-                        var placeholderEl = angular.element('<div in-view="$inview && increaseLimit()">' + placeholder + '</div>');
-                        var isolateScope = $scope.$new(true);
-
-                        function increaseLimit() {
+                    isolateScope.increaseLimit = function () {
+                        $scope.$apply(function () {
                             var bufferLength = getBufferLength();
 
                             limit *= 2;
-                            
+
                             if (limit >= bufferLength) {
                                 isolateScope.$destroy();
                                 $animate.leave(placeholderEl);
                                 placeholderVisible = false;
                             }
+                        });
+                    };
 
-                            waiting = false;
+                    var elSiblings = el.parent().children();
+                    var elLastSibling = elSiblings.length === 0 ? el : elSiblings.eq(-1);
+
+                    $animate.enter(placeholderEl, el.parent(), elLastSibling).then(function () {
+                        VisibilityService.whenVisible(placeholderEl, isolateScope, isolateScope.increaseLimit);
+                    });
+                    $compile(placeholderEl)(isolateScope);
+                    placeholderVisible = true;
+                }
+
+                // Only apply lazyRepeater if the threshold is smaller then the number of items and if the
+                // parameter lazy-if is true
+                if (limit < getBufferLength() && $parse(attrs.lazyIf)($scope) !== false) {
+                    addPlaceholder();
+
+                    $scope.getLazyLimit = function () {
+                        return limit;
+                    };
+
+                    $scope.$watch(getBufferLength, function (bufferLength) {
+                        if (limit < bufferLength && !placeholderVisible) {
+                            addPlaceholder();
                         }
+                    });
+                }
+            };
+        }
+    };
+}]);
 
-                        isolateScope.increaseLimit = function () {
-                            if (waiting) {
-                                return;
-                            }
-                            waiting = true;
+angular.module('ngLazyRender').service('VisibilityService', ['$interval', '$q', function ($interval, $q) {
+    var intervalPromise = void 0;
+    var watchingItems = [];
+    var idCounter = 0;
 
-                            if (delay) {
-                                $timeout(increaseLimit, delay);
-                            } else {
-                                increaseLimit();
-                            }
-                        };
+    // Code fully stolen from angular-inview. Thanks thenikso!
+    function getViewportHeight() {
+        var height, mode, ref;
+        height = window.innerHeight;
+        if (height) {
+            return height;
+        }
+        mode = document.compatMode;
+        if (mode || !(typeof $ !== "undefined" && $ !== null ? (ref = $.support) != null ? ref.boxModel : void 0 : void 0)) {
+            height = mode === 'CSS1Compat' ? document.documentElement.clientHeight : document.body.clientHeight;
+        }
+        return height;
+    }
 
-                        var elSiblings = el.parent().children();
-                        var elLastSibling = elSiblings.length === 0 ? el : elSiblings.eq(-1);
-
-                        $animate.enter(placeholderEl, el.parent(), elLastSibling).then(function () {
-                            // trigger in-view for other listeners
-                            angular.element(window).triggerHandler('checkInView');
-                        });
-                        $compile(placeholderEl)(isolateScope);
-                        placeholderVisible = true;
-                    }
-
-                    // Only apply lazyRepeater if the threshold is smaller then the number of items and if the
-                    // parameter lazy-if is true
-                    if (limit < getBufferLength() && $parse(attrs.lazyIf)($scope) !== false) {
-                        addPlaceholder();
-
-                        $scope.getLazyLimit = function () {
-                            return limit;
-                        };
-
-                        $scope.$watch(getBufferLength, function (bufferLength) {
-                            if (limit < bufferLength && !placeholderVisible) {
-                                addPlaceholder();
-                            }
-                        });
-                    }
-                };
+    // Code fully stolen from angular-inview. Thanks thenikso!
+    function getBoundingClientRect(element) {
+        var el, parent, top;
+        if (element.getBoundingClientRect != null) {
+            return element.getBoundingClientRect();
+        }
+        top = 0;
+        el = element;
+        while (el) {
+            top += el.offsetTop;
+            el = el.offsetParent;
+        }
+        parent = element.parentElement;
+        while (parent) {
+            if (parent.scrollTop != null) {
+                top -= parent.scrollTop;
             }
+            parent = parent.parentElement;
+        }
+        return {
+            top: top,
+            bottom: top + element.offsetHeight
         };
-    }]);
+    }
+
+    this._startWatching = function () {
+        if (!intervalPromise) {
+            intervalPromise = $interval(this._checkInView, 500, 0, false);
+        }
+    };
+
+    this._stopWatching = function () {
+        if (intervalPromise) {
+            $interval.cancel(intervalPromise);
+            intervalPromise = null;
+        }
+    };
+
+    // Code partially stolen from angular-inview. Thanks thenikso!
+    this._checkInView = function () {
+        var viewport = {
+            top: 0,
+            bottom: getViewportHeight()
+        };
+        var callbacks = [];
+
+        watchingItems.forEach(function (item) {
+            var bounds = getBoundingClientRect(item.element[0]);
+
+            if (bounds.top < viewport.bottom && bounds.bottom >= viewport.top) {
+                callbacks.push(item.callback);
+            }
+        });
+
+        callbacks.forEach(function (callback) {
+            callback();
+        });
+    };
+
+    this.whenVisible = function (element, scope, callback) {
+        var _this = this;
+
+        var itemId = idCounter++;
+
+        watchingItems.push({
+            element: element,
+            callback: callback,
+            scope: scope,
+            id: itemId
+        });
+
+        scope.$on('$destroy', function () {
+            watchingItems = watchingItems.filter(function (item) {
+                return item.id !== itemId;
+            });
+
+            if (watchingItems.length === 0) {
+                _this._stopWatching();
+            }
+        });
+
+        this._startWatching();
+    };
+}]);
