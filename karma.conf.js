83dd76585a8aa69127a292a7c4d1fda92eaf585d
Andre Duarte <andremacielduarte@gmail.com>
2016-10-31 13:09:11 +0000

    Fix Babel config for unit tests

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function(config) {
     ],
     preprocessors: {
       'src/**/*.js': ['coverage', 'babel'],
-      'test/**/*.js': ['babel']
+      'tests/**/*.js': ['babel']
     },
     reporters: ['coverage', 'spec'],
     colors: true,
